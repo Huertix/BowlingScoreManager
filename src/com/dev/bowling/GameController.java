99132547050fddd12a0e67d85b2d52fb8f971fb0
David Huerta <dhuerta23@gmail.com>
2016-04-20 18:14:52 +0200

    Adding comments to GameController

--- src/com/dev/bowling/GameController.java
+++ src/com/dev/bowling/GameController.java
@@ -92,26 +92,27 @@ public class GameController {
         for( int i = 0; i < frameList.size(); i++ ){
             Frame f = frameList.get(i);
             if(f.isStrike()){
+                // In case there are 2 strike in a row
                 if(index - i > 2){
                     Frame f1 = frameList.get(i + 1);
                     Frame f2 = null;
+                    // In case there is another strike ahead
                     if( f1.getThrow2() == null){
                         f2 = frameList.get(i + 2);
-                        f.setScore(10 + f1.getThrow1() + f2.getThrow1());
-                    } else {
-                        f.setScore(10 + f1.getThrow1() + f1.getThrow2());
+                        f.setScore(10 + f1.getThrow1() + f2.getThrow1()); // Eg. Throws {10, 10 , 1}
                     }
-                } else if(index - i > 1){
+                } else if(index - i > 1){ // In case there is a strike
                     Frame f1 = frameList.get(i + 1);
-                    f.setScore(10 + f1.getThrow1() + f1.getThrow2());
-                } else {
+                    f.setScore(10 + f1.getThrow1() + f1.getThrow2()); // Eg. Throws {10, 3 , 2}
+                } else { // Just Strike in the currentPosition
                     f.setScore(10 + f.getThrow1() + f.getThrow2());
                 }
             } else if(f.isSpare()){
+                // In case there is another spare ahead
                 if(index - i > 0){
                     Frame f1 = frameList.get(i + 1);
-                    f.setScore(10 + f1.getThrow1());
-                } else {
+                    f.setScore(10 + f1.getThrow1()); // Eg. Throws {8, 2 , 10}
+                } else { // Just spare in the currentPosition
                     f.setScore(10 + f.getThrow1());
                 }
             } else{
