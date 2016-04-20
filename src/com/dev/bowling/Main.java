b13f87fc62524a2d9e6e80ad3b92a10a39b8e0f9
David Huerta <dhuerta23@gmail.com>
2016-04-20 18:22:46 +0200

    Adding Main and jar

--- src/com/dev/bowling/Main.java
+++ src/com/dev/bowling/Main.java
@@ -1,7 +1,18 @@
 package com.dev.bowling;
 
-/**
- * Created by huertix on 20.04.16.
- */
 public class Main {
+
+    public static void main(String[] args){
+        GameController gc = new GameController();
+
+        if(args.length < 12 || args.length > 21) {
+            System.out.println("Input Error: Please review arguments");
+            System.out.println("E.g: java -jar BowlingGame.jar 5 2 8 1 10 3 2 10 10 1 6 10 2 6 10 4 5");
+        } else {
+            for (String arg: args) {
+                gc.newThrow(Integer.parseInt(arg));
+            }
+            gc.printScores();
+        }
+    }
 }
