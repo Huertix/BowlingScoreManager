ab1dfcbc635161f8fb4188445dd73ded43ad9f3d
David Huerta <dhuerta23@gmail.com>
2016-04-20 17:21:27 +0200

    Adding Strike test

--- test/TestGame.java
+++ test/TestGame.java
@@ -46,4 +46,44 @@ public class TestGame extends TestCase {
         assertEquals("TotalScore in Frame10 = 61", 61, gc.getScoreAtFrame(10));
         assertTrue("Game is over", gc.gameIsOver());
     }
+
+    @Test
+    public void testWithStrikes(){
+        GameController gc = new GameController();
+        gc.newThrow(5);
+        gc.newThrow(2);
+        assertEquals("Score Frame1 = 7", 7, gc.getScoreAtFrame(1));
+        gc.newThrow(8);
+        gc.newThrow(1);
+        assertEquals("Score Frame1 = 7", 7, gc.getScoreAtFrame(1));
+        assertEquals("Score Frame2 = 16", 16, gc.getScoreAtFrame(2));
+        gc.newThrow(10);
+        assertEquals("Score Frame1 = 7", 7, gc.getScoreAtFrame(1));
+        assertEquals("Score Frame2 = 16", 16, gc.getScoreAtFrame(2));
+        gc.newThrow(3);
+        gc.newThrow(2);
+        assertEquals("Score Frame1 = 7", 7, gc.getScoreAtFrame(1));
+        assertEquals("Score Frame2 = 16", 16, gc.getScoreAtFrame(2));
+        assertEquals("Score Frame3 = 31", 31, gc.getScoreAtFrame(3));
+        assertEquals("Score Frame4 = 36", 36, gc.getScoreAtFrame(4));
+        gc.newThrow(10);
+        assertEquals("Score Frame5 = 0", 0, gc.getScoreAtFrame(5));
+        gc.newThrow(10);
+        assertEquals("Score Frame6 = 0", 0, gc.getScoreAtFrame(6));
+        gc.newThrow(1);
+        gc.newThrow(6);
+        assertEquals("Score Frame5 = 57", 57, gc.getScoreAtFrame(5));
+        assertEquals("Score Frame6 = 74", 74, gc.getScoreAtFrame(6));
+        assertEquals("Score Frame7 = 81", 81, gc.getScoreAtFrame(7));
+        gc.newThrow(10);
+        assertEquals("Score Frame8 = 0", 0, gc.getScoreAtFrame(8));
+        gc.newThrow(2);
+        gc.newThrow(6);
+        assertEquals("Score Frame8 = 99", 99, gc.getScoreAtFrame(8));
+        assertEquals("Score Frame9 = 107", 107, gc.getScoreAtFrame(9));
+        gc.newThrow(4);
+        gc.newThrow(5);
+        assertEquals("Score Frame10 = 116", 116, gc.getScoreAtFrame(10));
+        assertTrue("Game is over", gc.gameIsOver());
+    }
 }
