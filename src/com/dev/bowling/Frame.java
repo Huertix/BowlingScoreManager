ad3e1e44a081755916223c549526e850b746f428
David Huerta <dhuerta23@gmail.com>
2016-04-20 17:22:03 +0200

    updating Class Frame

--- src/com/dev/bowling/Frame.java
+++ src/com/dev/bowling/Frame.java
@@ -4,34 +4,46 @@ package com.dev.bowling;
 public class Frame {
     private Integer throw1;
     private Integer throw2;
+    private Integer extraThrow;
     private int score;
+    private boolean isScorePending;
+    private boolean isStrike;
+    private boolean isSpare;
 
     public Frame(){
         score = 0;
+        isScorePending = true;
+        extraThrow = 0;
     }
 
+    public Integer getThrow1(){ return throw1; }
 
-    public Integer getThrow1(){
-        return throw1;
-    }
+    public void setThrow1(int throw1){ this.throw1 = throw1; }
 
-    public void setThrow1(int throw1){
-        this.throw1 = throw1;
-    }
+    public Integer getThrow2(){ return throw2; }
 
-    public Integer getThrow2(){
-        return throw2;
-    }
+    public void setThrow2(int throw2){ this.throw2 = throw2; }
 
-    public void setThrow2(int throw2){
-        this.throw2 = throw2;
-    }
+    public Integer getExtraThrow() { return extraThrow; }
 
-    public int getScore(){
-        return score;
-    }
+    public void setExtraThrow(int extraThrow) { this.extraThrow = extraThrow; }
+
+    public boolean isScorePending() { return isScorePending; }
+
+    public boolean isStrike() { return isStrike; }
+
+    public void setStrike(boolean strike) { this.isStrike = strike; }
+
+    public boolean isSpare() { return isSpare; }
+
+    public void setSpare(boolean spare) { this.isSpare = spare; }
+
+    public int getScore(){ return score; }
 
     public void setScore(int score){
         this.score = score;
+        this.isStrike = false;
+        this.isSpare = false;
+        this.isScorePending = false;
     }
 }
